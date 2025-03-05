477366a23cc4edaa005f4f32e6b38fe0b8df46ec
irampton <OrangeJedi@users.noreply.github.com>
2025-03-04 19:27:55 -0700

    made rtc class

--- RP2040/src/main.cpp
+++ RP2040/src/main.cpp
@@ -2,10 +2,16 @@
 #include <Wire.h>
 
 #include "Rotary.h"
+#include "DS3231_Wrapper.h"
 
 Rotary* r1 = new Rotary(9,10,8);
+DS3231_Wrapper myRTC;
 
 void setup() {
+    Wire.setSDA(0);
+    Wire.setSCL(1);
+    Wire.begin();
+
     Serial.begin( 9600 );
     r1->init();
     while (! Serial);
@@ -24,6 +30,7 @@ void loop() {
         Serial.println("CCW");
     }
     if(rotary_input[2]){
-        Serial.println("Rotary Button");
+        //Serial.println("Rotary Button");
+        myRTC.printTime();
     }
 }
